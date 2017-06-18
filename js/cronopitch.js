5d886e6c120c020650b33054bc3396051a91c629
alexandrevl <alexandrevl@gmail.com>
2017-06-17 21:22:41 -0300

    ajust folders and cache

--- js/cronopitch.js
+++ js/cronopitch.js
@@ -426,9 +426,9 @@ function setTimer(countDownDate) {
         if (continuous) {
             distance = now - countDownDate;
         }
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        //var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var minutes = hours * 60 + Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         if (seconds < 10) {
             seconds = "0" + seconds;
